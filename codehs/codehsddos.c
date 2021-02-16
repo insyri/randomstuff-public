042b8cbe15e2cf95ad31a1c78001e11970713390
insyri <nomamrcds@gmail.com>
2021-02-16 17:15:07 -0500

    Update codehsddos.c

--- codehs/codehsddos.c
+++ codehs/codehsddos.c
@@ -5,5 +5,6 @@ int main()
     int i;
     for(i = 0; i < 99999999999999999999999999999999999999999999999999; ++i){
         printf("152.142.175.21, %d\n", i);
+        /*this is the mcdonalds ip lol, no im not even joking search it up*/
     }
 }
